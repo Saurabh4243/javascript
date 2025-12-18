a68953267a6803b97e32c8edefb8141ed5609404
Saurabh Kothule <Saurabhkothule800@gmail.com>
2025-12-18 12:40:36 +0530

    Add files via upload

---
+++ promiss.js
@@ -0,0 +1,12 @@
+const getPromise = () => {
+    return new Promise((resolve, reject) => {
+        console.log("I am promise");
+        resolve("success");
+    });
+};
+
+let promise = getPromise();
+
+promise.then(() => {
+    console.log("fulfilled");
+});
